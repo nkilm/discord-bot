a8430de11fca222808e01d1f44bf69bebe6f8514
nkilm <nikhilmohitelhs@gmail.com>
2022-02-26 23:28:39 +0530

    Complete code for main.py

--- main.py
+++ main.py
@@ -1,36 +1,56 @@
+import discord
 from dotenv import load_dotenv 
 from os import environ
-import discord
+from requests import get
+from random import choice
 
 load_dotenv()
 
 TOKEN = environ.get('ABBOT_TOKEN')
 
+
 client = discord.Client()
 
+def get_quotes():
+  res = get("https://zenquotes.io/api/random")
+  quote_info = res.json()[0]
+  return f"{quote_info['q']} \n\t- {quote_info['a']}"
 
+def nasa_today():
+  try:
+    res = get(f"https://api.nasa.gov/planetary/apod?api_key={environ['NASA']}")
+    # print(res.json())
+    data = res.json()
+    return data["title"],data["date"],data["explanation"]
+  except:
+    return "Error with NASA API(Limit Exceeded)"
+
+  
+# registering the events
 @client.event
 async def on_ready():
-    print(f"Logged in - {client.user}")
-    # print("Logged in - {0.user}".format(client))
-    # print(client.user)
+    print(f"Logged in {client.user}\n")
 
 @client.event
-async def handle_msg(msg_obj):
-    username = str(msg_obj.author).split("#")[0]
-    msg = str(msg_obj.content)
-    channel = str(msg_obj.channel.name)
-    print(f"{username} : {msg} : ({channel}) ")
+async def on_message(msg):
+  # logging the info
+  # print(f"{msg.author}: {msg.content}")
 
-    # to prevent the bot from replying to itself 
-    if msg_obj.author == client.user:
-        return 
-    
-    if msg_obj.channel.name == "Testing Server for bots":
-        if msg.lower() == "hello":
-            await msg_obj.channel.send(f"Hello {username}")
-            return 
-        
+  #actual message from the chat
+  message = msg.content
+  if msg.author == client.user:
+    return
+
+  if (message.startswith("$nasa")):
+    title,date,desc = nasa_today()
+    await msg.channel.send(f"{date}\n\"{title}\"\n\t{desc}")
+  
+  if (message.startswith("$info")):
+    await msg.channel.send("Available Commands:\n $inspire\t$nasa\t$new\t$del\t$show")
+
+  if message.startswith("$inspire"):
+    await msg.channel.send(get_quotes())
+
 client.run(TOKEN)
 
 
